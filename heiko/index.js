9af39a20e430f5b48a403d691487d24fdd2dca8f
HeikoKraemer <github@myforce.net>
2021-12-17 00:34:05 +0100

    heiko cv cli added

---
+++ heiko/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+console.log(`
+Heiko Krämer
+
+40 years old father (daughter, 7) with overall 23 years of professional work experience in diverse roles and fields. 
+Focused on the telecommunications sector for the past nine years. Fast, continuous and motivated learner, emphatic listener and keen observer. 
+Just-do-it mentality. Salesforce enthusiast since 2012. Certified Admin, App Builder and Developer (PD1). 
+Strongly interested in process efficiency, simplification and automation.
+
+👨 LinkedIn:  https://www.linkedin.com/in/heikokraemer/
+🐻 Trailhead: https://trailblazer.me/id/hkrmer
+
+`);
