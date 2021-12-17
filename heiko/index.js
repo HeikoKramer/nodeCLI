62e5a97f6c51a0a7afcc53e841cad90a525307d0
HeikoKraemer <github@myforce.net>
2021-12-17 20:56:09 +0100

    cli-welcome added

--- heiko/index.js
+++ heiko/index.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
-const pkgJSON      = require('./package.json');
- 
-// Clears the console.
-clearConsole();
+const welcome = require('cli-welcome');
+const pkgJSON = require('./package.json');
 
-// Add a CLI Header
-console.log(`
-NAME: ${pkgJSON.name}
-VERSION: ${pkgJSON.version}
-DESCRIPTION: ${pkgJSON.description}
-`);
+// Add a CLI Welcome
+welcome({
+    title: `${pkgJSON.name}`,
+    tagLine: `${pkgJSON.description}`,
+    bgColor: `#6d1f56`,
+    color: `#ffffff`,
+    bold: true,
+    clear: true,
+    version: `${pkgJSON.version}`
+});
 
 console.log(`
 Heiko Krämer
