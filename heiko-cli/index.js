626d8e66e1d021e03c4dc2ff6f472a7933276535
HeikoKramer <github@myforce.net>
2021-12-18 16:09:43 +0100

    workSince function added, text & styling changed

--- heiko-cli/index.js
+++ heiko-cli/index.js
@@ -9,13 +9,13 @@ const chalk   = require('chalk');
 const log = console.log;
 
 // Colored Logos
-linkedIn   = chalk.bold.bgWhite.hex('#0A66C2')(' Linked') 
-           + chalk.white.bold.bgHex('#0A66C2')('in ');
-salesForce = chalk.white.bold.bgHex('#00A1E0')(' sales') 
+linkedIn   = chalk.bold.bgWhite.hex('#0A66C2')('Linked') 
+           + chalk.white.bold.bgHex('#0A66C2')('in');
+salesForce = chalk.white.bold.bgHex('#00A1E0')('sales') 
            + chalk.white.bold.italic.bgHex('#00A1E0')('f') 
-           + chalk.white.bold.bgHex('#00A1E0')('orce ');
-trailHead  = chalk.bgWhite.hex('#032E61')(' TRAILHEAD ');
-gitHub     = chalk.bgWhite.black.bold(' GitHub ');
+           + chalk.white.bold.bgHex('#00A1E0')('orce');
+trailHead  = chalk.bgWhite.hex('#032E61')('TRAILHEAD');
+gitHub     = chalk.bgWhite.black.bold('GitHub');
 
 // Add a CLI Welcome
 welcome({
@@ -31,16 +31,17 @@ welcome({
 log(`
 ${chalk.hex('#6d1f56').bold('Heiko Krämer')}
 
-${myAge()} years old father (daughter, 7) with overall 23 years of professional work experience in diverse roles and fields. 
-Focused on the telecommunications sector for the past nine years. Fast, continuous and motivated learner, emphatic listener and keen observer. 
-Just-do-it mentality. ${salesForce} enthusiast since 2012. Certified Admin, App Builder and Developer (PD1). 
-Strongly interested in process efficiency, simplification and automation.
+${chalk.hex('#48483E')(`${myAge()} years old father with overall ${workSince()} years of work experience in diverse roles and fields. 
+Main focus areas were print-media (7 years) and telecommunications (9 years).
+Motivated learner, emphatic listener and keen observer. Just-do-it mentality.`)} 
 
-👨 ${linkedIn}  ${chalk.dim.underline.blue(`https://www.linkedin.com/in/heikokraemer/`)}
+${salesForce} ${chalk.hex('#48483E').dim(`enthusiast since 2012. 
+Certified Admin, App Builder and Developer (PD1). 
+Strongly interested in process efficiency, simplification and automation.`)}
 
-🐙 ${gitHub}    ${chalk.dim.underline.blue(`https://github.com/HeikoKramer`)}
-
-🐻 ${trailHead} ${chalk.dim.underline.blue(`https://trailblazer.me/id/hkrmer`)}
+${linkedIn}  ${chalk.dim.underline.blue(`https://www.linkedin.com/in/heikokraemer/`)}
+${gitHub}    ${chalk.dim.underline.blue(`https://github.com/HeikoKramer`)}
+${trailHead} ${chalk.dim.underline.blue(`https://trailblazer.me/id/hkrmer`)}
 
 `);
 
@@ -52,6 +53,16 @@ function myAge () {
     thisDay   = new Date();
 
     const years = (thisDay.getFullYear() - birthDate.getFullYear()).toString();
+
+    return years;
+}
+
+// Calculate my years of work experience
+function workSince () {
+    startDate = new Date("08/01/1998");
+    thisDay   = new Date();
+
+    const years = (thisDay.getFullYear() - startDate.getFullYear()).toString();
 
     return years;
 }
