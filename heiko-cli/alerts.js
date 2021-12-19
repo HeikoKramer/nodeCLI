da68f5dd50fec197d3bba00f9c6af14f636434d0
HeikoKraemer <github@myforce.net>
2021-12-19 22:54:23 +0100

    custom alert name option added

--- heiko-cli/alerts.js
+++ heiko-cli/alerts.js
@@ -4,25 +4,27 @@ const sym   = require('log-symbols');
 module.exports = options => {
   const defaultOptions = {
     type: `error`,
-    msg: `No options provided`
+    msg: `No options provided`,
+    name: ``
   };
   const opts = {...defaultOptions, ...options};
-  const {type, msg} = opts;
+  const {type, msg, name} = opts;
+  const printName = name ? name : type.toUpperCase();
 
   if (type === `success`) {
-    console.log(`${sym.success} ${chalk.green.bold('SUCCESS:')} ${chalk.green(msg)}`)
+    console.log(`${sym.success} ${chalk.green.bold(`${printName}:`)} ${chalk.green(msg)}`)
   }
 
   if (type === `info`) {
-    console.log(`${sym.info} ${chalk.blue.bold('INFO:')} ${chalk.blue(msg)}`)
+    console.log(`${sym.info} ${chalk.blue.bold(`${printName}:`)} ${chalk.blue(msg)}`)
   }
 
   if (type === `warning`) {
-    console.log(`${sym.warning} ${chalk.yellow.bold('WARNING:')} ${chalk.yellow(msg)}`)
+    console.log(`${sym.warning} ${chalk.yellow.bold(`${printName}:`)} ${chalk.yellow(msg)}`)
   }
 
   if (type === `error`) {
-    console.log(`${sym.error} ${chalk.red.bold('ERROR:')} ${chalk.red(msg)}`)
+    console.log(`${sym.error} ${chalk.red.bold(`${printName}:`)} ${chalk.red(msg)}`)
   }
 
 }
