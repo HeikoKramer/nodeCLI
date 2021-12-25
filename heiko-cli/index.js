c13344a2dc8cae14f39fb9e7322c3a8b5201f201
HeikoKraemer <github@myforce.net>
2021-12-25 22:23:01 +0100

    meow added

--- heiko-cli/index.js
+++ heiko-cli/index.js
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
 
 // Required packages and files
+const meow        = require('meow');
 const alert       = require('./alerts');
 const init        = require('./utils/init');
 const handleError = require('cli-handle-error');
 const data        = require('./utils/data');
 
-
+const helpTest = `RUN npx heiko`;
+const options  = {
+    flags: {
+        social: {
+            type: 'boolean',
+            default: true,
+        },
+        disclaimer: {
+            type: 'boolean',
+            default: true,
+        }
+    }
+};
+const cli = meow(helpTest, options);
 
 (async () => {
     init(); 
@@ -15,4 +29,9 @@ const data        = require('./utils/data');
     console.log(data.social)
 
     alert({ type: 'info', msg: data.disclaimer, name: 'DISCLAIMER' });
+
+
+    alert({ type: 'info', msg: 'CLI DATA ↓' });
+    console.log('cli.input', cli.input);
+    console.log('cli.flags', cli.flags);
 })();
