4db0d6f0421dca6bb048a45f0cf4ca0df4ccc38e
HeikoKraemer <github@myforce.net>
2021-12-28 00:05:30 +0100

    spinner added (ora)

--- heiko-cli/utils/stats.js
+++ heiko-cli/utils/stats.js
@@ -1,21 +1,40 @@
 const axios       = require('axios');
 const handleError = require('cli-handle-error');
+const ora         = require('ora');
 
-const apiURL = `https://api.github.com/users/HeikoKramer`;
+const spinner = ora({ text: '' });
+const apiURL  = `https://api.github.com/users/HeikoKramer`;
 
 module.exports = async isDebug => {
 
+  
+  
+
   try {
+
+    spinner.start('fetching public repos …');
+
     const res  = await axios.get(apiURL);
     const publicRepos = res.data.public_repos;
+    
+    spinner.stop();
+  
     console.log(`
 Public Github Repos: ${publicRepos}
     `);
+
+    
+   
   } catch (error) {
     if (isDebug) {
       handleError(`API CALL FAILED`, error, true, false)
   } else {
       handleError(`API CALL FAILED`, error, false, false)
     }
+
+    
   }
+  
+  
+  
 }
