529b97e4af93faa05dee0555afe0ac08e4cdceb8
HeikoKramer <github@myforce.net>
2021-12-27 15:16:45 +0100

    boxen added

--- heiko-cli/utils/init.js
+++ heiko-cli/utils/init.js
@@ -2,6 +2,7 @@ const checkNode = require('cli-check-node');
 const unhandled = require('cli-handle-unhandled');
 const welcome   = require('cli-welcome');
 const pkgJSON   = require('../package.json');
+const boxen     = require('boxen');
 
 
 module.exports = (minimal, clear) => {
@@ -21,7 +22,7 @@ module.exports = (minimal, clear) => {
     }
 
     if (minimal) {
-        console.log(pkgJSON.name, pkgJSON.version);
+        console.log(boxen(`version: ${pkgJSON.version}`, { padding: 1, margin: 1, borderStyle: 'round', borderColor: 'yellow', title: `${pkgJSON.name}`, titleAlignment: 'center' }));
     }
     
     checkNode('12');
