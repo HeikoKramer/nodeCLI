b1352131ddcd27873f2f2a2f4bc7792e69820079
HeikoKramer <github@myforce.net>
2021-12-28 18:12:46 +0100

    codeblock formatted

--- heiko-cli/utils/stats.js
+++ heiko-cli/utils/stats.js
@@ -7,34 +7,21 @@ const apiURL  = `https://api.github.com/users/HeikoKramer`;
 
 module.exports = async isDebug => {
 
-  
-  
-
   try {
-
     spinner.start('fetching public repos …');
 
     const res  = await axios.get(apiURL);
     const publicRepos = res.data.public_repos;
     
     spinner.stop();
-  
-    console.log(`
-Public Github Repos: ${publicRepos}
-    `);
+    console.log(`Public Github Repos: ${publicRepos}`);
 
-    
-   
   } catch (error) {
+
     if (isDebug) {
       handleError(`API CALL FAILED`, error, true, false)
   } else {
       handleError(`API CALL FAILED`, error, false, false)
     }
-
-    
   }
-  
-  
-  
 }
