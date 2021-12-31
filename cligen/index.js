7d100c148f28dd9cff1d4d64814f737229efeebf
HeikoKramer <github@myforce.net>
2021-12-31 14:26:26 +0100

    basic boilerplate logic completed

--- cligen/index.js
+++ cligen/index.js
@@ -2,12 +2,21 @@
 const path = require('path');
 const copy = require('copy-template-dir');
 
-const vars   = { name: `heiko` };
+const vars = { 
+  name: `cli-img`,
+  description: `CLI to resize and optimize images`,
+  version: `0.0.0`
+};
+
 const inDir  = path.join(__dirname, `template`);
-const outDir = path.join(process.cwd(), `output`);
+const outDir = path.join(process.cwd(), vars.name);
 
 copy(inDir, outDir, vars, (err, createdFiles) => {
     if (err) throw err;
-    createdFiles.forEach(filePath => console.log(`Created ${filePath}`));
-    console.log('done!');
+    console.log(`\nCreating files in ./${vars.name}\n`);
+    createdFiles.forEach((filePath) => {
+      const fileName = path.basename(filePath);
+      console.log(`Created: ${fileName}`);
+    });
+    console.log(`\nSuccess – new project "${vars.name}" has been created!\n`);
   });
