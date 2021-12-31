1533bcbb49345281a2246fa1f3fcf25974222c57
HeikoKraemer <github@myforce.net>
2021-12-31 10:47:57 +0100

    basic logic added

--- cligen/index.js
+++ cligen/index.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+const path = require('path');
+const copy = require('copy-template-dir');
+
+const vars   = { name: `heiko` };
+const inDir  = path.join(__dirname, `template`);
+const outDir = path.join(process.cwd(), `output`);
+
+copy(inDir, outDir, vars, (err, createdFiles) => {
+    if (err) throw err;
+    createdFiles.forEach(filePath => console.log(`Created ${filePath}`));
+    console.log('done!');
+  });
