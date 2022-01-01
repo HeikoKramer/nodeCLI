954ea4ac9315a4919f7dcb146477402d04cc1c29
HeikoKramer <github@myforce.net>
2022-01-01 11:49:36 +0100

    output colors adjusted

--- cligen/index.js
+++ cligen/index.js
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
-const path        = require('path');
-const copy        = require('copy-template-dir');
-const init        = require('./utils/init');
-const ask         = require('./utils/ask');
+const path  = require('path');
+const copy  = require('copy-template-dir');
+const chalk = require('chalk');
+
+const init = require('./utils/init');
+const ask  = require('./utils/ask');
+
+// colors
+const b  = chalk.bold;
+const g  = chalk.green;
+const bg = chalk.bold.green;
+const gb = chalk.bold.green.inverse;
+const w  = chalk.white;
+const bw = chalk.bold.white;
+const d  = chalk.dim;
 
 (async() => {
   init();
@@ -48,16 +59,20 @@ const ask         = require('./utils/ask');
     authorUrl
     };
   
-  const inDir  = path.join(__dirname, `template`);
-  const outDir = path.join(process.cwd(), vars.name);
+  const outDir = vars.name;
+  const inDirPath  = path.join(__dirname, `template`);
+  const outDirPath = path.join(process.cwd(), outDir);
   
-  copy(inDir, outDir, vars, (err, createdFiles) => {
+  copy(inDirPath, outDirPath, vars, (err, createdFiles) => {
       if (err) throw err;
-      console.log(`\nCreating files in ./${vars.name}\n`);
+
+      console.log(bw(`\n  creating files in`), g(`./${outDir}\n`));
+
       createdFiles.forEach((filePath) => {
         const fileName = path.basename(filePath);
-        console.log(`Created: ${fileName}`);
+        console.log(g(`✔`), w(`created:`), g(fileName));
       });
-      console.log(`\nSuccess – new project "${vars.name}" has been created!\n`);
+
+      console.log(gb(`\n✔ SUCCESS`), w(`new project`), g(`${outDir}`), w(`has been created!\n`));
     });
 })();
