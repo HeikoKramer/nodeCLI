bc740e002a3e770dd12f29a7d1009a2605f3569b
HeikoKramer <github@myforce.net>
2022-01-01 09:43:06 +0100

    ask util added to take user input dynamically

--- cligen/index.js
+++ cligen/index.js
@@ -2,37 +2,43 @@
 const path        = require('path');
 const copy        = require('copy-template-dir');
 const init        = require('./utils/init');
-const { Input }   = require('enquirer');
-const to          = require('await-to-js').default;
-const handleError = require('cli-handle-error');
+const ask         = require('./utils/ask');
 
 (async() => {
-    init();
+  init();
 
-    const [err, name] = await to(
-        new Input({
-            message: `please enter CLI name`,
-            hint: `enter name here (camel case only)`
-        }).run()
-    );
-    handleError(`INPUT`, err);
+  const name = await ask({ 
+    message: `please enter CLI name`,
+    hint: `(camel case only)`,
+  });
 
-    const vars = { 
-      name,
-      description: `CLI to resize and optimize images`,
-      version: `0.0.0`
-    };
-    
-    const inDir  = path.join(__dirname, `template`);
-    const outDir = path.join(process.cwd(), vars.name);
-    
-    copy(inDir, outDir, vars, (err, createdFiles) => {
-        if (err) throw err;
-        console.log(`\nCreating files in ./${vars.name}\n`);
-        createdFiles.forEach((filePath) => {
-          const fileName = path.basename(filePath);
-          console.log(`Created: ${fileName}`);
-        });
-        console.log(`\nSuccess – new project "${vars.name}" has been created!\n`);
+  const description = await ask({ 
+    message: `please enter description`  
+  });
+
+  let version = await ask({ 
+    message: `please enter initial version`,
+    initial: `0.0.0`  
+  });
+
+  // if (version === '') { version = `0.0.0`}; 
+
+  const vars = { 
+    name,
+    description,
+    version
+  };
+  
+  const inDir  = path.join(__dirname, `template`);
+  const outDir = path.join(process.cwd(), vars.name);
+  
+  copy(inDir, outDir, vars, (err, createdFiles) => {
+      if (err) throw err;
+      console.log(`\nCreating files in ./${vars.name}\n`);
+      createdFiles.forEach((filePath) => {
+        const fileName = path.basename(filePath);
+        console.log(`Created: ${fileName}`);
       });
+      console.log(`\nSuccess – new project "${vars.name}" has been created!\n`);
+    });
 })();
