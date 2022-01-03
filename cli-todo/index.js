d0201e06873f9e8854089860d38b261cf6b32483
HeikoKramer <github@myforce.net>
2022-01-03 21:20:30 +0100

    make-dir added

--- cli-todo/index.js
+++ cli-todo/index.js
@@ -7,6 +7,13 @@
  * @author Heiko Krämer <https://heikokraemer.de>
  */ 
 
+const path    = require('path');
+const fs      = require('fs');
+const makeDir = require('make-dir');
+
+// Database
+const dbTodos = path.join(process.cwd(), `.todo/todos.json`);
+
 const init = require('./utils/init');
 const cli  = require('./utils/cli');
 const log  = require('./utils/log');
@@ -18,6 +25,12 @@ const { clear, debug } = flags;
 (async () => {
     init({ clear });
     input.includes(`help`) && cli.showHelp(0);
+
+    if(!fs.existsSync(dbTodos)) {
+        await makeDir(`.todo`);
+        process.chdir(`.todo`);
+        fs.writeFileSync(`todos.json`, `{}`);
+    }
 
     debug && log(flags);
 })();
