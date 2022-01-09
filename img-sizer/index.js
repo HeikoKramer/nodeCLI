335223728dd0543898cb9768be8c0dd554095dc3
HeikoKramer <github@myforce.net>
2022-01-09 17:45:59 +0100

    spinner & success message added

--- img-sizer/index.js
+++ img-sizer/index.js
@@ -7,15 +7,18 @@
  * @author Heiko Krämer <https://heikokraemer.de>
  */ 
 const resizeOptimizeImages = require('resize-optimize-images');
+const { yellow : y, green : g } = require('chalk');
 const globby = require('globby');
 const init   = require('./utils/init');
 const cli    = require('./utils/cli');
 const log    = require('./utils/log');
 const alert  = require('cli-alerts');
+const ora    = require('ora');
 
 const input = cli.input;
 const flags = cli.flags;
 const { clear, debug, source, width, quality } = flags;
+const spinner = ora({ text: `` });
 
 (async () => {
     init({ clear });
@@ -29,9 +32,10 @@ const { clear, debug, source, width, quality } = flags;
             quality: quality ? quality : 90
         };
     
+        spinner.start(`${y(`RUNNING`)} ${images.length} images are getting resized & optimized …`);
         await resizeOptimizeImages(options);
+        spinner.succeed(`${g(`COMPLETED`)} all images are resized & optimized!`);
 
-        imgDebug = images;
     } else {
         alert({
             type: `error`,
