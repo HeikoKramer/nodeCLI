22b39d03c7138d6dcd7b3135cfd065fd5a64b120
HeikoKramer <github@myforce.net>
2022-01-09 16:01:51 +0100

    basic image resize logic added

--- img-sizer/index.js
+++ img-sizer/index.js
@@ -6,18 +6,39 @@
  * 
  * @author Heiko Krämer <https://heikokraemer.de>
  */ 
-
-const init = require('./utils/init');
-const cli  = require('./utils/cli');
-const log  = require('./utils/log');
+const resizeOptimizeImages = require('resize-optimize-images');
+const globby = require('globby');
+const init   = require('./utils/init');
+const cli    = require('./utils/cli');
+const log    = require('./utils/log');
+const alert  = require('cli-alerts');
 
 const input = cli.input;
 const flags = cli.flags;
-const { clear, debug } = flags;
+const { clear, debug, source, width, quality } = flags;
 
 (async () => {
     init({ clear });
     input.includes(`help`) && cli.showHelp(0);
 
+    if (source) {
+        var images = await globby(source);
+        const options = {
+            images,
+            width: width ? width : 300,
+            quality: quality ? quality : 90
+        };
+    
+        await resizeOptimizeImages(options);
+
+        imgDebug = images;
+    } else {
+        alert({
+            type: `error`,
+            msg: `You forgot to specify a source.`
+        });
+    }
+    
+    debug && log(images);
     debug && log(flags);
 })();
